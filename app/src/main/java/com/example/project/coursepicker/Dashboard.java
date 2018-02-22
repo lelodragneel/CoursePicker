407e988e90d3a502448e0d41b80a88ccba002dac
lelodragneel <lelo.saiyan@gmail.com>
2018-02-22 13:16:47 -0400

    basic working dashboard add listeners add icons & bg

---
+++ app/src/main/java/com/example/project/coursepicker/Dashboard.java
@@ -0,0 +1,30 @@
+package com.example.project.coursepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void profileBtnListener(View view) {
+
+    }
+
+    public void calendarBtnListener(View view) {
+
+    }
+
+    public void coursesBtnListener(View view) {
+
+    }
+
+    public void settingsBtnListener(View view) {
+
+    }
+}
