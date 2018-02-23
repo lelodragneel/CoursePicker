376063dd1a2a0f560c0df05a21d610124d3c603f
Kenny <kennth_5@hotmail.com>
2018-02-23 15:57:12 -0400

    password checker and login check
    Junit test

---
+++ app/src/main/java/com/example/project/coursepicker/loginchecker.java
@@ -0,0 +1,107 @@
+package com.example.project.coursepicker;
+
+/**
+ * Created by Kenneth on 2/23/2018.
+ */
+
+public class loginchecker {
+
+	String passWord;
+
+public void setPassWord(String x){
+        passWord = x;
+        }
+
+//-----------------------------PASSWORD------------------------------------------------
+
+public boolean match(String x){			//check if password is direct match
+        if (!x.equals(passWord)){
+        return false;
+        }
+        return true;
+        }
+
+
+public boolean okLength(String x) {		//check length of password
+        if (x.length() < 8) {
+        return false;
+        }
+        return true;
+        }
+
+public boolean specChar(String x){		//check for at least 1 special character
+        for (int i=0; i < x.length(); i++){
+        int ascii = (int) x.charAt(i);
+        if (!(ascii >= 65 && ascii <= 90) && !(ascii >= 97 && ascii <= 122)){
+        return true;
+        }
+        }
+        return false;
+        }
+
+public boolean pwNumCheck(String x){		//check for at least 1 number character
+        for (int i=0; i < x.length(); i++){
+        int ascii = (int) x.charAt(i);
+        if (ascii >= 48 && ascii <= 57){
+        return true;
+        }
+        }
+        return false;
+        }
+
+public boolean caseCheck(String x){		//check for at least 1 upper case and 1 lower case character
+        boolean upper = false;
+        boolean lower = false;
+        for (int i=0; i < x.length(); i++){
+        int ascii = (int) x.charAt(i);
+        if (ascii >= 65 && ascii <= 90){
+        upper = true;
+        }
+        if (ascii >= 97 && ascii <= 122){
+        lower = true;
+        }
+        if (upper == true && lower == true){
+        return true;
+        }
+        }
+        return false;
+        }
+
+//-----------------------------USER NAME CASES------------------------
+
+public boolean userLength(String x){		//user name length must be 8 characters
+        if (x.length() == 8){
+        return true;
+        }
+        return false;
+
+        }
+
+public boolean letterCheck(String x){		//user names first two characters must be letters
+        Boolean condition1 = false;
+        Boolean condition2 = false;
+
+        int ascii = (int) x.charAt(0);
+        if ((ascii >= 65 && ascii <= 90) || ascii >= 97 && ascii <= 122){
+        condition1 = true;
+        }
+        ascii = (int) x.charAt(1);
+        if ((ascii >= 65 && ascii <= 90) || ascii >= 97 && ascii <= 122){
+        condition2 = true;
+        }
+        if (condition1 == true && condition2 == true){
+        return true;
+        }
+        return false;
+        }
+
+public boolean unNumCheck(String x){		//user names last six characters must be numbers
+        for (int i = x.length()-6; i < x.length(); i++){
+        int ascii = (int) x.charAt(i);
+        if (!(ascii >= 48 && ascii <= 57)){
+        return false;
+        }
+        }
+        return true;
+        }
+}
