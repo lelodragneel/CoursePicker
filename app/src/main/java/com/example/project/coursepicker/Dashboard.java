3267fe159a94b569b776e367361c878adebc91cb
lelodragneel <lelo.saiyan@gmail.com>
2018-02-23 16:27:23 -0400

    add animations

--- app/src/main/java/com/example/project/coursepicker/Dashboard.java
+++ app/src/main/java/com/example/project/coursepicker/Dashboard.java
@@ -3,7 +3,10 @@ package com.example.project.coursepicker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -11,22 +14,79 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // Profile touch listener
+        LinearLayout l1 = findViewById(R.id.linearLayout);
+        l1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.isActivated())
+                    v.setActivated(false);
+                else
+                    v.setActivated(true);
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // TODO: activity intent
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Calendar touch listener
+        LinearLayout l2 = findViewById(R.id.linearLayout2);
+        l2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.isActivated())
+                    v.setActivated(false);
+                else
+                    v.setActivated(true);
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // TODO: activity intent
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Courses touch listener
+        LinearLayout l3 = findViewById(R.id.linearLayout3);
+        l3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.isActivated())
+                    v.setActivated(false);
+                else
+                    v.setActivated(true);
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Intent intent = new Intent(v.getContext(), CoursesActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Options touch listener
+        LinearLayout l4 = findViewById(R.id.linearLayout4);
+        l4.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.isActivated())
+                    v.setActivated(false);
+                else
+                    v.setActivated(true);
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // TODO: activity intent
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
-    public void profileBtnListener(View view) {
-
-    }
-
-    public void calendarBtnListener(View view) {
-
-    }
-
-    public void coursesBtnListener(View view) {
-        Intent intent = new Intent(this, CoursesActivity.class);
-        startActivity(intent);
-    }
-
-    public void settingsBtnListener(View view) {
-
-    }
 }
