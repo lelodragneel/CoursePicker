7ed7f60ce014de97c6a7a74fe5143325b2b680ac
cgpaszkowski <polishstriker_12@live.ca>
2018-03-19 18:11:25 -0300

    precautionary commit 2

---
+++ app/src/main/java/changePassword.java
@@ -0,0 +1,21 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.project.coursepicker.R;
+
+/**
+ * Created by christian on 2018-03-19.
+ */
+
+public class changePassword extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+
+
+    }
+}
--- app/src/main/java/com/example/project/coursepicker/userProfile.java
+++ app/src/main/java/com/example/project/coursepicker/userProfile.java
@@ -1,7 +1,9 @@
 package com.example.project.coursepicker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -34,9 +36,9 @@ public class userProfile extends AppCompatActivity {
         setContentView(R.layout.user_profile);
 
         db_root = FirebaseDatabase.getInstance().getReference();
-        database_u_id = db_root.child("Users").child(session1.getID());    //------------------need reference to session user
-        database_u_name = db_root.child("Users").child(session1.getID());
-        database_u_email = db_root.child("Users").child(session1.getID());
+        database_u_id = db_root.child("Users").child("Ab123456");    //------------------need reference to session user
+        //database_u_name = db_root.child("Users").child("Ab123456");
+        //database_u_email = db_root.child("Users").child("Ab123456");
 
         user_ID = (TextView) findViewById(R.id.userID);
         user_nameView = (TextView) findViewById(R.id.userNameViewer);
@@ -44,6 +46,15 @@ public class userProfile extends AppCompatActivity {
         user_Email = (EditText) findViewById(R.id.userEmail);
         user_Phone = (EditText) findViewById(R.id.userPhone);
         user_Update = (Button) findViewById(R.id.updateProfileBtn);
+
+
+        user_ID.setText(database_u_id.toString());
+        user_nameView.setText(database_u_id.child("Name").toString());
+        user_Email.setText(database_u_id.child("Email").toString());
+        user_Phone.setText(database_u_id.child("Phone").toString());
+
+
+        startChangePassword(user_ChangePW);
 
 
         database_u_id.addValueEventListener(new ValueEventListener() {
@@ -79,5 +90,14 @@ public class userProfile extends AppCompatActivity {
 
     }
 
+
+    private void startChangePassword(View view){
+        Intent intent = new Intent(this, changePassword.class);
+        startActivity(intent);
+    }
+
+    private void updateProfileDetails(View view){
+
+    }
 
 }
