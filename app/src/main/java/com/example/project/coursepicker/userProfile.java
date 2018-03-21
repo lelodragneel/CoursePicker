9a3af55c0a9ff9cebd01354d3569855f5f127768
cgpaszkowski <polishstriker_12@live.ca>
2018-03-21 03:46:13 -0300

    precautionary commit 3

--- app/src/main/java/com/example/project/coursepicker/userProfile.java
+++ app/src/main/java/com/example/project/coursepicker/userProfile.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,15 +21,13 @@ import com.google.firebase.database.ValueEventListener;
 
 public class userProfile extends AppCompatActivity {
 
-    DatabaseReference db_root, database_u_id, database_u_name, database_u_email;
+    DatabaseReference db_root, database_u_id;
     private TextView user_ID;
     private TextView user_nameView;
     private TextView user_ChangePW;
     private EditText user_Email;
     private EditText user_Phone;
     private Button user_Update;
-    private Session session1;   //-------------------------is this correct way to call session ???
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +36,6 @@ public class userProfile extends AppCompatActivity {
 
         db_root = FirebaseDatabase.getInstance().getReference();
         database_u_id = db_root.child("Users").child("Ab123456");    //------------------need reference to session user
-        //database_u_name = db_root.child("Users").child("Ab123456");
-        //database_u_email = db_root.child("Users").child("Ab123456");
 
         user_ID = (TextView) findViewById(R.id.userID);
         user_nameView = (TextView) findViewById(R.id.userNameViewer);
@@ -48,56 +45,40 @@ public class userProfile extends AppCompatActivity {
         user_Update = (Button) findViewById(R.id.updateProfileBtn);
 
 
-        user_ID.setText(database_u_id.toString());
+        user_ID.setText(database_u_id.toString());               //---retrieve current User details & set in profile
         user_nameView.setText(database_u_id.child("Name").toString());
         user_Email.setText(database_u_id.child("Email").toString());
         user_Phone.setText(database_u_id.child("Phone").toString());
 
-
-        startChangePassword(user_ChangePW);
-
-
-        database_u_id.addValueEventListener(new ValueEventListener() {
+        user_Update.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot_1) {
-                String u_id = dataSnapshot_1.getValue().toString();
-                user_ID.setText(u_id);
-
-                database_u_name = db_root.child("Users").child(session1.getID()).child("Name");  //--------------need reference to session user
-                user_nameView = (TextView) findViewById(R.id.userNameViewer);
-
-                database_u_name.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot_2) {
-                        String u_name = dataSnapshot_2.getValue().toString();   //gets userName <------ need to add reference to "session user"
-                        user_ID.setText(u_name);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+            public void onClick(View view) {
+                if (user_ID.getText().toString().trim().length() != 0 && user_Phone.getText().toString().trim().length() != 0){
+                    updateProfileDetails(user_Email.toString(), user_Phone.toString());
+                }
             }
         });
 
-
+        user_ChangePW.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startChangePassword();
+            }
+        });
 
     }
 
 
-    private void startChangePassword(View view){
+    private void updateProfileDetails(String email, String phone){
+        database_u_id.child("Email").setValue(email);
+        database_u_id.child("Phone").setValue(phone);
+        Toast.makeText(this,"Update Successful", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void startChangePassword(){
         Intent intent = new Intent(this, changePassword.class);
         startActivity(intent);
-    }
-
-    private void updateProfileDetails(View view){
-
     }
 
 }
