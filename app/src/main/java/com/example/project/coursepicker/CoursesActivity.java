f2d994a44f735442fe26f46a1e61091554177d30
Michael Gosnell <michaeljgosnell@gmail.com>
2018-04-10 10:34:23 -0300

    importing digitKeyListener once and for all

--- app/src/main/java/com/example/project/coursepicker/CoursesActivity.java
+++ app/src/main/java/com/example/project/coursepicker/CoursesActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.DigitsKeyListener;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
